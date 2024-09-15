124e3bbf79bad7eb6f236d644c605470582ac82d
Sathish <seesathish@gmail.com>
2024-09-15 23:36:15 +0530

    Basket.API distributed cache implemented.

--- src/Services/Basket/Basket.API/GlobalUsing.cs
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -1,13 +1,17 @@
 ﻿
-global using Basket.API.Models;
-global using BuildingBlocks.CQRS;
-global using Carter;
-global using MediatR;
-global using Mapster;
-global using FluentValidation;
-global using BuildingBlocks.Behaviors;
-global using BuildingBlocks.Exceptions;
-global using Basket.API.Exceptions;
-global using Marten;
 global using Basket.API.Data;
+global using Basket.API.Exceptions;
+global using Basket.API.Models;
+global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Exceptions;
 global using BuildingBlocks.Exceptions.Handler;
+global using Carter;
+global using FluentValidation;
+global using HealthChecks.UI.Client;
+global using Mapster;
+global using Marten;
+global using MediatR;
+global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+global using Microsoft.Extensions.Caching.Distributed;
+global using System.Text.Json;
