d28972c78181daf0e286fa87785d8ea8c990ba4f
Sathish <seesathish@gmail.com>
2024-09-15 08:08:04 +0530

    Cross cutting concerns implemented for Catalog API

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -1,8 +1,16 @@
 ﻿
-global using Carter;
-global using Mapster;
-global using MediatR;
-global using Marten;
+global using BuildingBlocks.Behaviors;
 global using BuildingBlocks.CQRS;
-global using Catalog.API.Models;
+global using BuildingBlocks.Exceptions.Handler;
+global using Carter;
+global using Catalog.API.Data;
 global using Catalog.API.Exceptions;
+global using Catalog.API.Models;
+global using FluentValidation;
+global using HealthChecks.UI.Client;
+global using Mapster;
+global using Marten;
+global using Marten.Pagination;
+global using MediatR;
+global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
