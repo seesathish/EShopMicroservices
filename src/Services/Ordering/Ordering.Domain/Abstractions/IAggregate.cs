3087d51f52df926381ac4930f451b12485e1cf29
Sathish <seesathish@gmail.com>
2024-09-17 10:29:49 +0530

    Ordering domain abstraction implemented.

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IAggregate.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Principal;
+
+namespace Ordering.Domain.Abstractions;
+
+public interface IAggregate<T> : IAggregate, IEntity<T>
+{
+}
+
+public interface IAggregate : IEntity
+{
+    IReadOnlyList<IDomainEvent> DomainEvents { get; }
+    IDomainEvent[] ClearDomainEvents();
+}
